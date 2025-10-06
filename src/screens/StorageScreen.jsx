f29f90da3d43d4501dce37e977f556033122d644
Vince AG <49834750+EdocEdoc@users.noreply.github.com>
2025-10-06 22:06:21 +0800

    feat #4: Folder Stucture and React Native Navigation (#5)
    * feat: code owners
    
    * test: test push PR to dev (#1)
    
    * feat: initial folder structure and navigatio

---
+++ src/screens/StorageScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const StorageScreen = () => {
+  return (
+    <View>
+      <Text>StorageScreen</Text>
+    </View>
+  );
+};
+
+export default StorageScreen;
+
+const styles = StyleSheet.create({});
